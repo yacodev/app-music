df711a961f6e14bebeecfee32ab3013d2b9ef7e9
Carlos yaco <cyaco33@gmail.com>
2021-12-17 17:55:07 -0500

    implemented mixins

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,19 +1,53 @@
 <template>
 <div class="container">
   <div class="columns">
-    <div class="column is-5 is-offset-4">
-      <pm-track :track="track"></pm-track>
+    <div class="column is-3 has-text-centered">
+      <figure class="media-left">
+        <p class="image">
+          <img :src="track.album.images[0].url" alt="">
+        </p>
+        <p>
+          <button class="button is-primary is-large">
+            <span class="icon" @click="selectTrack"></span>
+          </button>
+        </p>
+      </figure>
+    </div>
+    <div class="column is-8">
+      <div class="panel">
+        <div class="panel-heading">
+          <h1 class="title">{{ track.name }}</h1>
+        </div>
+        <div class="panel-block">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <ul v-for="(v, k) in track" :key="k">
+                  <li>
+                    <strong>{{ k }}:&nbsp;</strong>
+                    <span>{{ v }}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <nav class="level">
+              <div class="level-left">
+                <button class="level-item"></button>
+              </div>
+            </nav>
+          </article>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 </template>
 <script>
 import trackService from '../services/track'
-import PmTrack from './Track.vue'
+import trackMixin from '../mixins/track'
 
 export default {
-  components: { PmTrack },
-
+  mixins: [trackMixin],
   data () {
     return {
       track: {}
