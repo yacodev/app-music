026a6b75661a0977b6ed0390cb1ee4ea6edc8f32
Carlos yaco <cyaco33@gmail.com>
2021-12-17 17:27:10 -0500

    add filter to convert time

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import App from './App.vue'
 import '../node_modules/bulma/css/bulma.css'
 import EventBus from './plugins/event-bus'
 import routes from './routes'
+import msToMm from './filters/ms-to-mm'
+import blur from './directives/blur'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(EventBus)
+Vue.use(msToMm)
+Vue.use(blur)
 
 const router = new VueRouter({
   routes,
